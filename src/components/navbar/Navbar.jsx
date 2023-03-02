20120702b48894f319e2f7331cb825161eee7369
jazzyyss <jaspreetbrar1636@gmail.com>
2023-03-02 02:57:27 -0500

    added basic dialog box for login functionality, just the skeleton

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,20 +1,28 @@
+import {useState} from 'react'
 import { useNavigate } from 'react-router-dom'
 import './navbar.css'
 
 const Navbar = ({status}) => {
     const navigate = useNavigate()
+    const [toggleLogin,setToggleLogin] = useState(false)
 
-    return(
+    return(<>
+        <div className={`lgnDia ${!toggleLogin?'':'active'}`} onClick={_=> setToggleLogin(!toggleLogin)}>
+            <div className="lgn" onClick={e=> e.stopPropagation()}>
+
+            </div>
+        </div>
+            
         <div className="navbarContainer">
-            <div className="logo">Task<span>Tracker</span></div>
+            <div className="logo" onClick={_=>console.log(toggleLogin)}>Task<span>Tracker</span></div>
             {status!=='regis' && (
                 <div className="buttonsss">
-                    <button className="buttonss loginBtn" onClick={_=>navigate('/login')}>Login</button>
+                    <button className="buttonss loginBtn" onClick={_=> setToggleLogin(!toggleLogin)}>Login</button>
                     <button className="buttonss registerBtn" onClick={_=>navigate('/register')}>Register</button>
                 </div>
             )}
         </div>
-    )
+        </>)
 }
 
 export default Navbar
